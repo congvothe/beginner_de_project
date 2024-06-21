fc80f57d71226fcb94204a97efdfa781598fcf82
cong <congvothe@gmail.com>
2024-06-21 10:53:22 +0700

    update version

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 5.55.0"
     }
     redshift = {
       source  = "brainly/redshift"
-      version = "1.0.2"
+      version = "1.7.0"
     }
   }
 
